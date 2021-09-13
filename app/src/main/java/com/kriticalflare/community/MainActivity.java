e95fc6d175ee2e11d87c8d0dbff07437f08e260f
kriticalflare <42350771+kriticalflare@users.noreply.github.com>
2021-09-13 22:43:48 +0530

    add bottom nav drawer using bottomsheet

--- app/src/main/java/com/kriticalflare/community/MainActivity.java
+++ app/src/main/java/com/kriticalflare/community/MainActivity.java
@@ -1,15 +1,51 @@
 package com.kriticalflare.community;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.splashscreen.SplashScreen;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.View;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.kriticalflare.community.databinding.ActivityMainBinding;
+import com.kriticalflare.community.databinding.BottomNavSheetBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private BottomSheetBehavior bottomSheetBehavior;
+    private BottomNavSheetBinding navSheetBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         SplashScreen.Companion.installSplashScreen(this);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View root = binding.getRoot();
+        navSheetBinding = binding.bttmNav;
+        bottomSheetBehavior = BottomSheetBehavior.from(navSheetBinding.bottomNavigationContainer);
+        TypedValue tv = new TypedValue();
+        if (getTheme().resolveAttribute(android.R.attr.actionBarSize, tv, true))
+        {
+            int actionBarHeight = TypedValue.complexToDimensionPixelSize(tv.data,getResources().getDisplayMetrics());
+            bottomSheetBehavior.setPeekHeight(actionBarHeight);
+        }
+        navSheetBinding.navIcon.setOnClickListener(v -> {
+           toggleBottomSheet(bottomSheetBehavior);
+        });
+
+        navSheetBinding.myToolbar.setOnClickListener(v -> {
+           toggleBottomSheet(bottomSheetBehavior);
+        });
+        setContentView(root);
+    }
+
+    private void toggleBottomSheet(BottomSheetBehavior bottomSheetBehavior){
+        if(bottomSheetBehavior.getState() != BottomSheetBehavior.STATE_EXPANDED){
+            bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+        } else {
+            bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+        }
     }
 }
