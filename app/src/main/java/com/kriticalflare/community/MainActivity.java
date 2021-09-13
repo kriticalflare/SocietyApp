1f193a6bb5b0ef2272976bb689fefdc5cb42bf54
kriticalflare <42350771+kriticalflare@users.noreply.github.com>
2021-09-14 03:57:03 +0530

    More integration between navigation and bottomsheet
    add transparent scrim and sync nav destination with toolbar title

--- app/src/main/java/com/kriticalflare/community/MainActivity.java
+++ app/src/main/java/com/kriticalflare/community/MainActivity.java
@@ -2,16 +2,23 @@ package com.kriticalflare.community;
 
 import android.os.Bundle;
 import android.util.TypedValue;
+import android.view.MenuItem;
 import android.view.View;
 
 import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.splashscreen.SplashScreen;
 import androidx.core.view.WindowCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.navigation.NavigationView;
 import com.kriticalflare.community.databinding.ActivityMainBinding;
 import com.kriticalflare.community.databinding.BottomNavSheetBinding;
 
@@ -22,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
     private BottomSheetBehavior<View> bottomSheetBehavior;
     private BottomNavSheetBinding navSheetBinding;
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,6 +69,64 @@ public class MainActivity extends AppCompatActivity {
             toggleBottomSheet(bottomSheetBehavior);
         });
 
+        NavHostFragment navHostFragment =
+                (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        if(navHostFragment != null){
+            navController = navHostFragment.getNavController();
+        }
+
+        navController.addOnDestinationChangedListener((navController, navDestination, bundle) -> {
+            switch (navDestination.getId()){
+                case R.id.eventsFragment:
+                    navSheetBinding.navigationView.setCheckedItem(R.id.nav_events);
+                    navSheetBinding.myToolbar.setTitle("Events");
+                    break;
+                case R.id.homeFragment:
+                    navSheetBinding.myToolbar.setTitle("Home");
+                    break;
+                case R.id.loginFragment:
+                    navSheetBinding.myToolbar.setTitle("Login");
+                    break;
+                case R.id.registerFragment:
+                    navSheetBinding.myToolbar.setTitle("Register");
+                    break;
+            }
+
+        });
+
+        navSheetBinding.navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.nav_events:
+                        navController.popBackStack(R.id.eventsFragment, false);
+                        toggleBottomSheet(bottomSheetBehavior);
+                        break;
+                    case R.id.nav_meetings:
+                        navController.navigate(R.id.action_global_homeFragment);
+                        toggleBottomSheet(bottomSheetBehavior);
+                        break;
+                    case R.id.nav_complaints:
+                        navController.navigate(R.id.action_global_loginFragment);
+                        toggleBottomSheet(bottomSheetBehavior);
+                        break;
+                    case R.id.nav_parking:
+                        navController.navigate(R.id.action_global_registerFragment);
+                        toggleBottomSheet(bottomSheetBehavior);
+                        break;
+                    case R.id.nav_emergency:
+                        navController.navigate(R.id.action_global_homeFragment);
+                        toggleBottomSheet(bottomSheetBehavior);
+                        break;
+                }
+                return true;
+            }
+        });
+
+        binding.scrim.setOnClickListener( v -> {
+            toggleBottomSheet(bottomSheetBehavior);
+        });
+
         OnBackPressedCallback closeBottomSheetCallback = new OnBackPressedCallback(
                 false
         ) {
@@ -78,8 +144,10 @@ public class MainActivity extends AppCompatActivity {
                 switch (newState) {
                     case BottomSheetBehavior.STATE_COLLAPSED:
                         closeBottomSheetCallback.setEnabled(false);
+                        binding.scrim.setVisibility(View.GONE);
                         break;
                     case BottomSheetBehavior.STATE_EXPANDED:
+                        binding.scrim.setVisibility(View.VISIBLE);
                         closeBottomSheetCallback.setEnabled(true);
                         break;
                 }
